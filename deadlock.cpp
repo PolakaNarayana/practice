2710325a382c7e214a6b0da3a13ba53774040bc1
PolakaNarayana <139432998+PolakaNarayana@users.noreply.github.com>
2023-07-28 14:39:27 +0530

    Add files via upload

---
+++ deadlock.cpp
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int wt[10],tat[10],bt[10],i,n,q,k,j;
+	printf("enter the integer number");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		print("the no of integer numbers",i);
+		scanf("%d",&&i);
+	}
+	for(i=1;i<=n;i++)
+	{
+		for(j=i;j<=n;j++)
+		{
+			if(i[0]==j[0])
+			{
+				if(j[i]==1)
+				{
+					printf("enter the burst time of integer");
+					scanf("%d"&&j);
+				}
+				else
+				{
+					return break;
+				}else if{
+					k=bt[i];
+					bt[i]=bt[j];
+					bt[j]=k;
+					p=wt[i];
+				}
+				awt=tt/float(n)
+				tt=wt+awt
+				printf("the total waiting time of fifo",t)
+				bt=bt[i]+bt[j];
+				awt=wt+tt;
+			}
+			awt=awt/n;
+			att=att/n;
+			printf("average turn around time",awt);
+			printf("average burst time",awt);
+		}
+	}
+}
